4b87240ff23a14a38b0c3809d7ca186adf4db683
Christopher Pinski <christopherpinski@gmail.com>
2022-07-27 09:37:32 -0500

    Adding a few more build options to optimize the build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://financialmodelastro.pages.dev',
   output: 'server',
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: 'file'
+  },
   adapter: cloudflare(),
   integrations: [tailwind()]
 });
