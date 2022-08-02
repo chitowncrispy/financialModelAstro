1d5e329280eb99f4ec619fd509d940f8b3019fd5
Christopher Pinski <christopherpinski@gmail.com>
2022-08-02 15:17:35 -0500

    Updating astro and @astrojs/cloudflare to fix cloudflare deploy issues

--- src/pages/placeholder.ts
+++ src/pages/placeholder.ts
@@ -1,12 +1,1 @@
-import { atom } from 'nanostores';
-
-export const singletonCartOpen = atom(false);
-
-export const isCartOpen = () => {
-   return singletonCartOpen.get();
-}
-
-export const customSetCartOpen = (cartOpen) => {
-   singletonCartOpen.set(cartOpen);
-   console.log("placeholderSet: " + singletonCartOpen.get());
-}
+export {}
